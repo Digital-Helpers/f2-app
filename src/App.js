0297dd63cd6a3195c70febcdff8c916fe104f11a
deanovision <rodeanfraser@gmail.com>
2019-10-04 08:37:55 -0400

    removed firebase and setup auth0

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
-import React from 'react';
-import './App.css';
-import SignUp from "./components/SignUp"
+// src/App.js
 
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import * as firebase from "firebase/app";
+import React from "react";
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./components/react-auth0-wrapper";
 
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
-import "firebase/analytics";
-
-// Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
-
-// TODO: Replace the following with your app's Firebase project configuration
 
 function App() {
+  const { loading } = useAuth0();
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyD9-XsU8yx4rGGKmyAm6qzWTzlWPCs-ZGk",
-    authDomain: "f2app-43a0e.firebaseapp.com",
-    databaseURL: "https://f2app-43a0e.firebaseio.com",
-    projectId: "f2app-43a0e",
-    storageBucket: "",
-    messagingSenderId: "728738468560",
-    appId: "1:728738468560:web:9b396b66371303007e84d5",
-    measurementId: "G-X69J6R0XM4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    );
+  }
 
   return (
     <div className="App">
-      <SignUp />
+      <header>
+        <NavBar />
+      </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// src/index.js
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "./components/react-auth0-wrapper";
 
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
 
+ReactDOM.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_DOMAIN}
+    client_id={process.env.REACT_APP_CLIENT_ID}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+>
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
